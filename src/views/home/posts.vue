658fc4c6ae9caf6c0f3b59aea3cab7ac45daa729
oscar <oscar@brandme.la>
2022-07-13 13:20:29 -0500

    vamos por mas

--- src/views/home/posts.vue
+++ src/views/home/posts.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="col-9 mx-auto">
 
-    <b-card :img-src="post.images" :img-alt="`Image-${post.caption}`" img-top class="mb-5" v-for="(post, index) in publications" :key="index">
-      <b-card-text>
+    <b-card 
+      border-variant="warning"
+      :img-src="post.images" 
+      :img-alt="`Image-${post.caption}`" 
+      img-top 
+      class="card-body bg-dark text-white mb-5" 
+      v-for="(post, index) in publications" :key="index">
+        <div class="d-flex">
+          <b-icon class="icon" icon="heart"/>
+          <b-icon  class="icon" icon="chat"/>
+          <b-icon  class="icon" icon="envelope"/>
+          <b-icon class="icon" icon="bookmark"/>
+        </div> 
+      <b-card-text class="m-2">
         {{post.caption}}
 
       </b-card-text>
@@ -34,5 +46,16 @@ export default {
 </script>
 
 <style>
+.icon{
+  margin:5px;
+  font-size: 18px;
+}
+.icon:hover{
+  color: orange;
+}
+.card-body{
+  padding: 0%;
+}
+
 
 </style>
