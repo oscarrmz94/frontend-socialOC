10ca9c0cc990899ee843feb18de3513180bd573f
Crisitan Ramon Hernandez Ordoñez <chino@MacBook-Air-de-Crisitan.local>
2022-07-16 08:49:25 -0500

    main

--- src/services/main.js
+++ src/services/main.js
@@ -3,20 +3,6 @@ import config from './config'
 import store from '@/store/index'
 
 export default {
-    dashboard() {
-        return axios.get(`${config.api_route}dashboard/posts/`,
-            {
-                headers: {
-                    'auth-token': store.state.token,
-                    'Content-Type': 'application/json'
-                }
-            }
-        ).then((res) => {
-            return res.data.data.user
-        }).catch((error) => {
-            return error.response
-        });
-    },
     getFollowersPosts(uuid) {
         return axios.get(`${config.api_route}dashboard/posts/get-posts-followers/${uuid}`,
             {
