190ad6dfd7e7a1bf212d4335784855020d5f4ae5
Cristian Hernandez <chinorubikguitar@hotmail.com>
2022-07-17 20:15:52 -0500

    dots actions, one element at post, confirmDialog

--- src/main.js
+++ src/main.js
@@ -2,13 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VuejsDialog from 'vuejs-dialog';
+//import VuejsDialogMixin from 'vuejs-dialog/dist/vuejs-dialog-mixin.min.js'; // only needed in custom components
+import 'vuejs-dialog/dist/vuejs-dialog.min.css';
 
+// include the default style
+
+// Tell Vue to install the plugin.
 import { BootstrapVue, IconsPlugin, BootstrapVueIcons } from 'bootstrap-vue'
 
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(VuejsDialog);
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
--- src/views/home/posts.vue
+++ src/views/home/posts.vue
@@ -1,99 +1,170 @@
 <template>
-  <div class="col-9 mx-auto">
+  <div class="col-8 mx-auto">
     <div v-for="(post, index) in publications" :key="index">
-      <b-card 
-        v-if="!utils.isVideo(post.images)"
+      <b-card
         border-variant="warning"
-        :img-src="post.images" 
-        :img-alt="`Image-${post.caption}`" 
-        img-top 
-        class="card-body bg-dark text-white mb-5" >
-          <div class="d-flex">
-            <b-icon class="icon" icon="heart"/>
-            <b-icon  class="icon" icon="chat"/>
-            <b-icon  class="icon" icon="envelope"/>
-            <b-icon class="icon" icon="bookmark"/>
-          </div> 
-        <b-card-text class="m-2">
-          {{post.caption}}
-
-        </b-card-text>
-        <template #footer>
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </template>
-      </b-card>
-
-      <b-card 
-        v-else
-        border-variant="warning"
-        class="card-body bg-dark text-white mb-5" >
-         <video
+        :img-alt="`Image-${post.caption}`"
+        class="card-body bg-dark text-white mb-5"
+      >
+        <div class="d-flex justify-content-between align-items-center p-2">
+          <div>
+            <b-avatar
+              class="b-avatar-list me-2"
+              :src="post.user_profile_image ? post.user_profile_image : ''"
+            />
+            <router-link
+              :to="{ name: 'Profile', params: { uuid: post.user_uuid } }"
+              class="text-white text-decoration-none router-link"
+            >
+              <span>{{ post.user_name }}</span>
+            </router-link>
+          </div>
+          <div>
+            <b-icon
+              icon="three-dots-vertical"
+              class="dots"
+              @click="show_actions = true"
+            />
+          </div>
+        </div>
+        <b-img
+          :src="post.images"
+          v-if="!utils.isVideo(post.images)"
+          class="col-12"
+        />
+        <video
+          v-else
           :src="post.images"
           autoplay
           muted
           controls
           class="video-main-post"
-        >Un canvas aqui</video>
+        />
 
         <div class="d-flex">
-          <b-icon class="icon" icon="heart"/>
-          <b-icon  class="icon" icon="chat"/>
-          <b-icon  class="icon" icon="envelope"/>
-          <b-icon class="icon" icon="bookmark"/>
-        </div> 
+          <b-icon class="icon" icon="heart" />
+          <b-icon class="icon" icon="chat" />
+          <b-icon class="icon" icon="envelope" />
+          <b-icon class="icon" icon="bookmark" />
+        </div>
         <b-card-text class="m-2">
-          {{post.caption}}
-
+          {{ post.caption }}
         </b-card-text>
         <template #footer>
           <small class="text-muted">Last updated 3 mins ago</small>
         </template>
       </b-card>
-    
     </div>
 
+    <b-modal
+      v-model="show_actions"
+      centered
+      hide-footer
+      hide-header
+      body-class="modal-actions-body"
+      class="modal-actions"
+      size="sm"
+      content-class="modal-actions-content"
+    >
+      <div class="h5 button-modal first m-0" @click="deletePost()">Delete</div>
+      <div class="h5 button-modal m-0">Unfollow</div>
+      <div class="h5 button-modal m-0">Go to post</div>
+      <div class="h5 button-modal m-0">Share to</div>
+      <div class="h5 button-modal m-0">Copy link</div>
+      <div class="h5 button-modal last m-0">Cancel</div>
+    </b-modal>
   </div>
 </template>
 
 <script>
-import utils from '@/libs/utils'
+import utils from "@/libs/utils";
 
 export default {
-  components: {
-    
-  },
+  components: {},
   data() {
     return {
-      utils
-    }
+      utils,
+      show_actions: false
+    };
   },
   props: {
     publications: {
       type: Array,
-      required: true
+      required: true,
+    },
+  },
+  methods: {
+    deletePost() {
+      this.show_actions = false;
+
+      this.$dialog.confirm('Are you sure that You want to delete this post ?')
+        .then(() => {
+            // This will be triggered when user clicks on proceed
+          console.log('Yeah delete this post')
+        })
+        .catch(() => {
+            // This will be triggered when user clicks on cancel
+        });
     }
   },
-
   created() {
-    console.log(this.publications)
-  }
-}
+    console.log(this.publications, 'hse');
+  },
+};
 </script>
 
 <style>
-.icon{
-  margin:5px;
+.icon {
+  margin: 5px;
   font-size: 18px;
 }
-.icon:hover{
+.icon:hover {
   color: orange;
 }
-.card-body{
+.card-body {
   padding: 0%;
 }
 .video-main-post {
   width: 100%;
   height: 400px;
 }
-
+.dropdown-points .btn-link,
+.dropdown-points a {
+  color: white;
+}
+.dots:hover {
+  cursor: pointer;
+}
+.button-modal {
+  text-align: center;
+  background-color: gray;
+  height: auto;
+  color: white;
+  padding: 1em;
+}
+.first {
+  border-top-left-radius: 25px !important;
+  border-top-right-radius: 25px !important;
+}
+.last {
+  border-bottom-left-radius: 25px !important;
+  border-bottom-right-radius: 25px !important;
+}
+.button-modal:hover {
+  background-color: orange;
+  cursor: pointer;
+}
+.first:hover {
+  background: red !important;
+}
+.modal-actions {
+  bottom: 10em;
+}
+.modal-actions-body {
+  padding: 0px !important;
+  border-radius: 50px !important;
+}
+.modal-actions-content {
+  background-color: transparent !important;
+}
 </style>
